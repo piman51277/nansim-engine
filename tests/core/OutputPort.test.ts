277b2d063fd0c604ee565b589ca9a4e6e43e247d
piman51277 <piman@piman.dev>
2025-01-15 18:43:57 -0500

    add testing for missing condition

--- tests/core/OutputPort.test.ts
+++ tests/core/OutputPort.test.ts
@@ -1,5 +1,6 @@
 import { Network } from "../../src/core/Network";
 import { OutputPort } from "../../src/core/OutputPort";
+import { WireValue } from "../../src/core/WireValue";
 
 test("constructor with bind", () => {
     const network = new Network(0, 16, []);
@@ -29,4 +30,13 @@ test("setBinding", () => {
     port.setBinding({ id: 0, val: network });
     expect(port.bind.id).toBe(0);
     expect(port.bind.val).toBe(network);
+});
+
+test("set on no binding", () => {
+    const port = new OutputPort(1);
+    expect(port.bind.id).toBe(-1);
+    expect(port.bind.val).toBe(null);
+
+    //it should allow it, but nothing should happen
+    port.set(WireValue.unknown(16));
 });
