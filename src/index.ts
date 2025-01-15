3574f3cb9b8be42fdb71bb33f249074727035f86
piman51277 <piman@piman.dev>
2025-01-15 18:44:36 -0500

    add missing tests for short circuit and infinite loops

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { Engine } from "./core/Engine";
+import { EngineRuntimeError } from "./core/errors/EngineRuntimeError";
+import { ExtraInputError } from "./core/errors/ExtraInputError";
+import { InfiniteLoopError } from "./core/errors/InfiniteLoopError";
 import { InputPort } from "./core/InputPort";
+import { InvalidInputError } from "./core/errors/InvalidInputError";
+import { MissingInputError } from "./core/errors/MissingInputError";
 import { Module } from "./core/Module";
 import { Network } from "./core/Network";
 import { OutputPort } from "./core/OutputPort";
+import { ShortCircuitError } from "./core/errors/ShortCircuitError";
 import { WireValue } from "./core/WireValue";
 
 //These exports are mainly for type checking inputs,
@@ -15,5 +21,17 @@ export const Core = {
     EngineWireValue: WireValue,
     Engine: Engine
 };
+
+//errors
+export const Errors = {
+    EngineRuntimeError,
+    ExtraInputError,
+    InfiniteLoopError,
+    MissingInputError,
+    InvalidInputError,
+    ShortCircuitError
+};
+
+//types
 export type { IWireValue as EngineWireValue, RawBinding as EngineRawBinding, RawInputPort as EngineRawInputPort, RawOutputPort as EngineRawOutputPort, Setable as EngineSetable, ModuleTickFn as EngineModuleTickFn, RawModule as EngineRawModule, RawNetwork as EngineRawNetwork, RawObject as EngineRawObject } from './types';
 export { ModuleType as EngineModuleType, ObjectTypes as EngineObjectTypes } from "./types";
--- tests/core/Engine.test.ts
+++ tests/core/Engine.test.ts
@@ -7,6 +7,8 @@ import { InvalidInputError } from "../../src/core/errors/InvalidInputError";
 import { linkNet } from '../../src/io/linkNet';
 import { MissingInputError } from "../../src/core/errors/MissingInputError";
 import type { RawObject } from "../../src/types";
+import { ShortCircuitError } from "../../src/core/errors/ShortCircuitError";
+import { InfiniteLoopError } from "../../src/core/errors/InfiniteLoopError";
 
 /**
  * Creates a sample (raw) network with two inputs, a copy module, and two outputs
@@ -58,6 +60,78 @@ function createSampleNet0(): RawObject[] {
         out1
     ];
 }
+
+/**
+ * Creates a sample (raw) network that is capable of triggering a short circuit
+ * @returns {*}  {RawObject[]}
+ */
+function createShortCircuitNetwork0(): RawObject[] {
+
+    //two inputs
+    const input0 = createRawInputModule(0, 1, 16);
+    const port0 = createRawOutputPort(1, 16, 2);
+    const net0 = createRawNetwork(2, 16, [10]);
+
+    const input1 = createRawInputModule(3, 4, 16);
+    const port1 = createRawOutputPort(4, 16, 5);
+    const net1 = createRawNetwork(5, 16, [11]);
+
+    //2 -> 2 copy module
+    const iPort1 = createRawInputPort(10, 16, 12);
+    const iPort2 = createRawInputPort(11, 16, 12);
+    const module = createRawCopyModule(12, [10, 11], [13, 14]);
+    const oPort1 = createRawOutputPort(13, 16, 20);
+    const oPort2 = createRawOutputPort(14, 16, 20);
+
+    //two outputs
+    const oNet1 = createRawNetwork(20, 16, [21]);
+    const iPort3 = createRawInputPort(21, 16, 22);
+    const out0 = createRawOutputModule(22, 21, 16);
+
+    return [
+        input0,
+        port0,
+        net0,
+        input1,
+        port1,
+        net1,
+        iPort1,
+        iPort2,
+        module,
+        oPort1,
+        oPort2,
+        oNet1,
+        iPort3,
+        out0
+    ];
+}
+
+/**
+ * Creates a sample (raw) network that will trigger an infinite loop
+ * @returns {*}  {RawObject[]}
+ */
+function createInfiniteLoop0(): RawObject[] {
+
+    //one input
+    const input0 = createRawInputModule(0, 1, 16);
+    const port0 = createRawOutputPort(1, 16, 2);
+    const net0 = createRawNetwork(2, 16, [11]);
+
+    //1 -> 1 copy module
+    const iPort2 = createRawInputPort(11, 16, 12);
+    const module = createRawCopyModule(12, [11], [13]);
+    const oPort1 = createRawOutputPort(13, 16, 2);
+
+    return [
+        input0,
+        port0,
+        net0,
+        iPort2,
+        module,
+        oPort1
+    ];
+}
+
 
 test("constructor", () => {
     const rawNet = createSampleNet0();
@@ -163,5 +237,79 @@ test("evaluate", () => {
     expect((engine.getObject(25) as OutputModule)?.get().data).toEqual(data1);
 });
 
-//TODO: create circuits for short circuit evaluation
+test("evaluate short circuit 1", () => {
+    const rawNet = createShortCircuitNetwork0();
+    const linked = linkNet(rawNet);
+    const engine = new Engine(linked);
+
+    const inputs = new Map<number, Uint8Array>();
+    const data0 = new Uint8Array([0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]);
+    const data1 = new Uint8Array([1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]);
+
+    inputs.set(0, data0);
+    inputs.set(3, data1);
+
+    engine.passInputs(inputs);
+    expect(() => {
+        engine.evaluate();
+    }).toThrow(ShortCircuitError);
+
+    try {
+        engine.evaluate();
+    }
+    catch (e: any) {
+        expect(e.affects.length).toBe(1);
+        expect(e.affects[0].id).toBe(20);
+        expect(e.message).toEqual("Runtime Error affecting [20]: Short circuit detected");
+    }
+});
+
+test("evaluate overlapping non-short circuit", () => {
+    const rawNet = createShortCircuitNetwork0();
+    const linked = linkNet(rawNet);
+    const engine = new Engine(linked);
+
+    const inputs = new Map<number, Uint8Array>();
+    const data0 = new Uint8Array([0, 0, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2]);
+    const data1 = new Uint8Array([0, 0, 0, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0]);
+    const expectedOut = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
+    inputs.set(0, data0);
+    inputs.set(3, data1);
+
+    engine.passInputs(inputs);
+    expect(() => {
+        engine.evaluate();
+    }).not.toThrow();
+
+    expect((engine.getObject(22) as OutputModule)?.get().data).toEqual(expectedOut);
+});
+
+test("evaluate infinite loop 1", () => {
+    const rawNet = createInfiniteLoop0();
+    const linked = linkNet(rawNet);
+
+    const engine = new Engine(linked);
+
+    const input = new Uint8Array([0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]);
+    const inputs = new Map<number, Uint8Array>();
+    inputs.set(0, input);
+
+    engine.passInputs(inputs);
+    expect(() => {
+        engine.evaluate();
+    }).toThrow(InfiniteLoopError);
+
+    try {
+        engine.evaluate();
+    }
+    catch (e: any) {
+        expect(e.affects.length).toBe(1);
+        expect(e.affects[0].id).toBe(2);
+        expect(e.message).toEqual("Runtime Error affecting [2]: Infinite loop detected");
+    }
+});
+
+
+
 //TODO: create circuits for infinite loops
